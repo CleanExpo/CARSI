ababe6868f029ed2797fbde29961323f06cbe71a
Rana Muzamil <mmlrana00@gmail.com>
2026-03-24 08:41:29 +0500

    chore(web): remove legacy app paths (batch 14/28)

--- apps/web/components/landing/index.ts
+++
@@ -1,4 +0,0 @@
-export { AnimatedHero, AnimatedStats, AnimatedCard, AnimatedSection } from './AnimatedHero';
-export { MobileNav } from './MobileNav';
-export { PublicNavbar } from './PublicNavbar';
-export { PublicFooter } from './PublicFooter';
