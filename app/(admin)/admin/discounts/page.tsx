25b0ab5efaba48f45853c30fc26948614b1e3450
Rana Muzamil <mmlrana00@gmail.com>
2026-04-03 22:30:37 +0500

    Add /admin/discounts page.

---
+++ app/(admin)/admin/discounts/page.tsx
@@ -0,0 +1,7 @@
+import { AdminDiscountsClient } from '@/components/admin/AdminDiscountsClient';
+
+export const dynamic = 'force-dynamic';
+
+export default function AdminDiscountsPage() {
+  return <AdminDiscountsClient />;
+}
